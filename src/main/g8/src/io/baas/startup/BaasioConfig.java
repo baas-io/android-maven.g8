31ca523a172c14553e7554c3fa8074acdd0ac70f
Sangbeom Kim <wangtao@kthcorp.com>
2012-12-04 17:50:19 +0900

    Initial commit

---
+++ src/main/g8/src/io/baas/startup/BaasioConfig.java
@@ -0,0 +1,10 @@
+package io.baas.startup;
+
+public class BaasioConfig {
+    public static final String BAASIO_URL = "$baas_api_url$";
+
+    public static final String ORGANIZATION_ID = "$baas_id$"; // member ID(organization id)
+
+    public static final String APPLICATION_ID = "$baas_backend_app$";
+
+}
